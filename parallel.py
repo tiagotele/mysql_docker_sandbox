481ee12d86c2bd263f5dd2c3618b64d027bd3cb9
Tiago Melo <tiagotele@gmail.com>
2024-08-17 08:28:06 -0300

    Improving parallel

--- parallel.py
+++ parallel.py
@@ -8,36 +8,60 @@ config = {
     'user': 'root',
     'password': '1234',
     'host': 'localhost',
-    'port': 3307,
+    'port': 3306,
     'database': 'test',
     'pool_name': 'mypool',
     'pool_size': 10  # Ensure this matches the number of threads or more
 }
+# config = {
+#     "user": "caylent_user",
+#     "password": "[Rent@lbe@stC@ylent]",
+#     "host": "qa-instance-aurora-caylent-clone-dms-0-cluster.cluster-cy4xvmuy53uf.us-east-1.rds.amazonaws.com",
+#     "port": 3306,
+#     "database": "rb_dev",
+#     "pool_name": "mypool",
+#     "pool_size": 32,  # Ensure this matches the number of threads or more
+# }
 
 # List of tables for which you want to run SELECT COUNT(*)
-tables = ["people", "companies", "cars", "products", "orders", "employees", "departments", "cities", "countries", "books"]
+tables = [
+    "SELECT COUNT(1) FROM test.people;",
+    "SELECT COUNT(1) FROM test.companies;",
+    "SELECT COUNT(1) FROM test.cars;",
+    "SELECT COUNT(1) FROM test.products;",
+    "SELECT COUNT(1) FROM test.orders;",
+    "SELECT COUNT(1) FROM test.employees;",
+    "SELECT COUNT(1) FROM test.departments;",
+    "SELECT COUNT(1) FROM test.cities;",
+    "SELECT COUNT(1) FROM test.countries;",
+    "SELECT COUNT(1) FROM test.books;",
+    ]
 
 
 
 # Function to execute the SELECT COUNT(*) query
-def run_query(table):
+def run_query(conn_config, query):
+
     cnx = None
     cursor = None
-    random_int = random.randint(1,3)
-    print(f"Running table {table} taking {random_int} seconds \n")
-    time.sleep(random_int)
     try:
-        cnx = mysql.connector.connect(**config)
+        start = time.time()
+        cnx = mysql.connector.connect(**conn_config)
         cursor = cnx.cursor()
 
-        query = f"SELECT COUNT(*) FROM {table}"
         cursor.execute(query)
         result = cursor.fetchone()
 
-        return table, result[0]
+        end = time.time()
+        total = end - start
+        print(
+            f"\nQuery = {query}.\nTotal = {result[0]}. Query execution time: {total:.3f}"
+        )
+
+        return query, result[0]
 
     except mysql.connector.Error as err:
-        return table, f"Error: {err}"
+        return query, f"Error: {err}"
 
     finally:
         if cursor:
@@ -45,22 +69,27 @@ def run_query(table):
         if cnx:
             cnx.close()
 
+
 # Execute queries in parallel using ThreadPoolExecutor
 def execute_queries_in_parallel(tables):
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        future_to_table = {executor.submit(run_query, table): table for table in tables}
+    print("Starting queries")
+    with ThreadPoolExecutor(max_workers=32) as executor:
+        future_to_table = {executor.submit(run_query, config, table): table for table in tables}
 
         for future in as_completed(future_to_table):
             table = future_to_table[future]
             try:
-                data = future.result()
-                print(f"Table: {data[0]}, Count: {data[1]}")
+                data = future.result(timeout=10)
+                # print(f"Query: {data[0]}, Count: {data[1]}")
+                print(f"DONE = {data}")
             except Exception as exc:
-                print(f"Table: {table} generated an exception: {exc}")
+                print(f"Query: {table} generated an exception: {exc}")
+
 
 if __name__ == "__main__":
-    s=time.time()
+    s = time.time()
     execute_queries_in_parallel(tables)
-    e=time.time()
-    total_time=e-s
+    e = time.time()
+    total_time = e - s
     print(f"Total time = {total_time:.3f}")
+    print("END")
