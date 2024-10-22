536e8f6e3760ce6d3b1f4f32ed2d084e39ecd068
Tiago <tiagotele@gmail.com>
2024-10-22 18:34:06 -0300

    Refactoring parallel script

--- src/parallel.py
+++ src/parallel.py
@@ -1,30 +1,29 @@
 import time
-from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from queries import run_query, QUERIES_FIRST_PHASE, TOP_TABLES
 from parallel_query import execute_queries_in_parallel
 from connector_settings import connections
 
 
-def print_with_indent(message:str, indent_level=0):
+def print_with_indent(message: str, indent_level=0):
     lines = message.split("\n")
     for line in lines:
         indent = "    " * indent_level  # Each indent level adds 4 spaces
         print(f"{indent}{line}")
 
+
 def join_tuple_elements(t):
     return ".".join(t)
 
-def compare_lists(
-    source_list: list = [],
-    destiny_list: list = [],
-    source_name: str = "blue",
-    destiny_name: str = "green",
-):
 
+def compare_lists(
+        source_list: list = [],
+        destiny_list: list = [],
+        source_name: str = "blue",
+        destiny_name: str = "green",
+):
     set_l1 = set(source_list)
     set_l2 = set(destiny_list)
-
 
     common = set_l1 & set_l2
     only_in_l1 = set_l1 - set_l2
@@ -32,7 +31,6 @@ def compare_lists(
 
     # Generate the output
     output = []
-
 
     # Add common entries
     for item in common:
@@ -52,6 +50,7 @@ def compare_lists(
         output_content = output_content + "\n" + item[0] + "," + item[1] + "," + item[2]
     return output_content
 
+
 def fetch_list_from_set(current_set):
     return list(map(lambda x: x[0], current_set))
 
@@ -60,14 +59,16 @@ def fetch_dict_from_set(current_set):
     a = list(map(lambda x: {x[0]: x[1]}, current_set))
     return sorted(a, key=lambda d: next(iter(d)))
 
+
 def extract_dict_from_list(l: set):
     output = {}
     for i in l:
         output[i[0]] = i[1]
     return output
 
+
 def compare_dicts(
-    dict_a: dict, dict_b: dict, source_name: str = "blue", destiny_name: str = "green"
+        dict_a: dict, dict_b: dict, source_name: str = "blue", destiny_name: str = "green"
 ) -> str:
     all_keys = set(dict_a.keys()).union(set(dict_b.keys()))
     result = f"key,{source_name},{destiny_name},result"
@@ -95,6 +96,7 @@ def tuples_list_are_equal(l1: list = [], l2: list = []) -> bool:
         equal_result = True
     return equal_result
 
+
 def compare_output_general_phase(output_src: dict, output_dest: dict, show_diff: bool):
     all_keys = set(output_src.keys()).union(set(output_dest.keys()))
     for key in sorted(all_keys):
@@ -118,57 +120,19 @@ def compare_output_general_phase(output_src: dict, output_dest: dict, show_diff:
                     1,
                 )
 
+
 def parser_tuples_to_str(d: dict):
     result = {}
     for k, v in d.items():
         result[k] = [(tuple(str(item) for item in tpl)) for tpl in v]
-    return  result
+    return result
 
-if __name__ == "__main__":
-    
-    # ################################################################
-    # # FIRST PHASE
-    # ################################################################
-    
-    print(f"first phase")
-    s = time.time()
+def query_and_show_result(querie_src, querie_dest, connection_src, connection_dest):
     output_src, output_dest = execute_queries_in_parallel(
-        QUERIES_FIRST_PHASE,
-        QUERIES_FIRST_PHASE,
-        connections["config1"],
-        connections["config2"],
-    )
-    e = time.time()
-    total_time = e - s
-    print("-----------")
-    print(f"Total time for queries = {total_time:.3f}")
-
-    general_data_source = {name: output for name, query, output in sorted(output_src)}
-    general_data_destiny = {name: output for name, query, output in sorted(output_dest)}
-
-    # ################################################################
-    # # FIRST PHASE
-    # ################################################################
-    compare_output_general_phase(
-        output_src=general_data_source, output_dest=general_data_destiny, show_diff=True
-    )
-
-    # ################################################################
-    # # SECOND PHASE
-    # ################################################################
-    
-    print(f"second phase")
-    top_tables_src = run_query(connections["config1"], "TOP_TABLES", TOP_TABLES)
-    top_tables_dest = run_query(connections["config2"], "TOP_TABLES", TOP_TABLES)
-    
-    top_queries_dict_src = {f"{schema}.{table}": f"SELECT COUNT(1) FROM {schema}.{table};" for schema, table in top_tables_src[2]}
-    top_queries_dict_dest = {f"{schema}.{table}": f"SELECT COUNT(1) FROM {schema}.{table};" for schema, table in top_tables_dest[2]}
-    
-    output_src, output_dest = execute_queries_in_parallel(
-        top_queries_dict_src,
-        top_queries_dict_dest,
-        connections["config1"],
-        connections["config2"],
+        querie_src,
+        querie_dest,
+        connection_src,
+        connection_dest
     )
 
     general_data_source = {name: output for name, query, output in sorted(output_src)}
@@ -180,4 +144,19 @@ if __name__ == "__main__":
         output_src=general_data_source, output_dest=general_data_destiny, show_diff=True
     )
 
-    
+
+if __name__ == "__main__":
+
+    # FIRST PHASE
+    query_and_show_result(QUERIES_FIRST_PHASE, QUERIES_FIRST_PHASE, connections["config1"], connections["config2"])
+
+    top_tables_src = run_query(connections["config1"], "TOP_TABLES", TOP_TABLES)
+    top_tables_dest = run_query(connections["config2"], "TOP_TABLES", TOP_TABLES)
+
+    top_queries_dict_src = {f"{schema}.{table}": f"SELECT COUNT(1) FROM {schema}.{table};" for schema, table in
+                            top_tables_src[2]}
+    top_queries_dict_dest = {f"{schema}.{table}": f"SELECT COUNT(1) FROM {schema}.{table};" for schema, table in
+                             top_tables_dest[2]}
+
+    # SECOND PHASE
+    query_and_show_result(top_queries_dict_src, top_queries_dict_dest, connections["config1"], connections["config2"])
