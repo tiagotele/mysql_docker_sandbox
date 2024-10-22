d2f2ad741143279f1fcabb6acd7a178f3d8c4bd3
Tiago <tiagotele@gmail.com>
2024-10-22 13:47:22 -0300

    Refactoring code

--- src/diff_output.py
+++ src/diff_output.py
@@ -1,62 +1,101 @@
-from parallel import tuples_list_are_equal, SHOW_DIFF, show_diff, fetch_dict_from_set, fetch_list_from_set, print_with_indent;
+from parallel import tuples_list_are_equal, print_with_indent
 
-output_source = {'QUERY_01_VERSIONS': [('5.7.41',)],
-                 'QUERY_02_VARIABLES': [
-                                        ('explicit_defaults_for_timestamp', 'OFF'),
-                                        ('slave_rows_search_algorithms', 'TABLE_SCAN,INDEX_SCAN'),
-                                        ('innodb_open_files', '4000'), ('innodb_optimize_fulltext_only', 'OFF'),
-                                        ],
-                 'QUERY_03_SCHEMAS_LIST': [('information_schema',), ('mysql',), ('performance_schema',), ('sys',),
-                                           ('test',), ('test2',)],
-                 'QUERY_04_LIST_TABLES': [('people',), ('books',),],
-                 'QUERY_06_LIST_CONSTRAINTS': [], 'QUERY_07_LIST_INDEXES': [], 'QUERY_08_LIST_PARTITIONS': [],
-                 'QUERY_09_LIST_STORED_PROCEDURES': [],
-                 'QUERY_10_LIST_TRIGGERS': [('sys_config_insert_set_user', 'INSERT', 'sys_config'),
-                                            ('sys_config_update_set_user', 'UPDATE', 'sys_config')],
-                 'QUERY_11_LIST_USERS_AND_HOSTS': [('root', '%'), ('healthchecker', 'localhost'),
-                                                   ('mysql.session', 'localhost'), ('mysql.sys', 'localhost'),
-                                                   ('root', 'localhost')],
-                 'QUERY_12_LIST_USERS_AND_ITS_PERMISSIONS': [('root',), ('healthchecker',), ('mysql.session',),
-                                                             ('mysql.sys',), ('root',)]}
-output_destiny = {'QUERY_01_VERSIONS': [('8.3.0',)],
-                  'QUERY_02_VARIABLES': [('explicit_defaults_for_timestamp', 'ON'),
-                                        ('innodb_open_files', '4000'),],
-                  'QUERY_03_SCHEMAS_LIST': [('information_schema',), ('mysql',), ('performance_schema',), ('sys',),
-                                            ('test',), ('test2',)],
-                  'QUERY_04_LIST_TABLES': [('people',), ('books2',),],
-                  'QUERY_06_LIST_CONSTRAINTS': [], 'QUERY_07_LIST_INDEXES': [], 'QUERY_08_LIST_PARTITIONS': [],
-                  'QUERY_09_LIST_STORED_PROCEDURES': [],
-                  'QUERY_10_LIST_TRIGGERS': [('sys_config_insert_set_user', 'INSERT', 'sys_config'),
-                                             ('sys_config_update_set_user', 'UPDATE', 'sys_config')],
-                  'QUERY_11_LIST_USERS_AND_HOSTS': [('root', '%'), ('mysql.infoschema', 'localhost'),
-                                                    ('mysql.session', 'localhost'), ('mysql.sys', 'localhost'),
-                                                    ('root', 'localhost')],
-                  'QUERY_12_LIST_USERS_AND_ITS_PERMISSIONS': [('root',), ('mysql.infoschema',), ('mysql.session',),
-                                                              ('mysql.sys',), ('root',)]}
-
-#
-# OUTPUT 1
-# k1 = v1
-# k2 = v2
-# k3 = v3
-#
-# OUTPUT 2
-# k1 = v1
-# k2 = v1
-# k4 = v2
-#
-# FINAL DIFF
-# k1  v1  v2  equals
-# k2  v2  v1  diff
-# k3  v3  none diff
-# k4  none v4 diff
+output_source = {
+    "QUERY_01_VERSIONS": [("5.7.41",)],
+    "QUERY_02_VARIABLES": [
+        ("explicit_defaults_for_timestamp", "OFF"),
+        ("slave_rows_search_algorithms", "TABLE_SCAN,INDEX_SCAN"),
+        ("innodb_open_files", "4000"),
+        ("innodb_optimize_fulltext_only", "OFF"),
+    ],
+    "QUERY_03_SCHEMAS_LIST": [
+        ("information_schema",),
+        ("mysql",),
+        ("performance_schema",),
+        ("sys",),
+        ("test",),
+        ("test2",),
+    ],
+    "QUERY_04_LIST_TABLES": [
+        ("people",),
+        ("books",),
+    ],
+    "QUERY_06_LIST_CONSTRAINTS": [],
+    "QUERY_07_LIST_INDEXES": [],
+    "QUERY_08_LIST_PARTITIONS": [],
+    "QUERY_09_LIST_STORED_PROCEDURES": [],
+    "QUERY_10_LIST_TRIGGERS": [
+        ("sys_config_insert_set_user", "INSERT", "sys_config"),
+        ("sys_config_update_set_user", "UPDATE", "sys_config"),
+    ],
+    "QUERY_11_LIST_USERS_AND_HOSTS": [
+        ("root", "%"),
+        ("healthchecker", "localhost"),
+        ("mysql.session", "localhost"),
+        ("mysql.sys", "localhost"),
+        ("root", "localhost"),
+    ],
+    "QUERY_12_LIST_USERS_AND_ITS_PERMISSIONS": [
+        ("root",),
+        ("healthchecker",),
+        ("mysql.session",),
+        ("mysql.sys",),
+        ("root",),
+    ],
+}
+output_destiny = {
+    "QUERY_01_VERSIONS": [("8.3.0",)],
+    "QUERY_02_VARIABLES": [
+        ("explicit_defaults_for_timestamp", "ON"),
+        ("innodb_open_files", "4000"),
+    ],
+    "QUERY_03_SCHEMAS_LIST": [
+        ("information_schema",),
+        ("mysql",),
+        ("performance_schema",),
+        ("sys",),
+        ("test",),
+        ("test2",),
+    ],
+    "QUERY_04_LIST_TABLES": [
+        ("people",),
+        ("books2",),
+    ],
+    "QUERY_06_LIST_CONSTRAINTS": [],
+    "QUERY_07_LIST_INDEXES": [],
+    "QUERY_08_LIST_PARTITIONS": [],
+    "QUERY_09_LIST_STORED_PROCEDURES": [],
+    "QUERY_10_LIST_TRIGGERS": [
+        ("sys_config_insert_set_user", "INSERT", "sys_config"),
+        ("sys_config_update_set_user", "UPDATE", "sys_config"),
+    ],
+    "QUERY_11_LIST_USERS_AND_HOSTS": [
+        ("root", "%"),
+        ("mysql.infoschema", "localhost"),
+        ("mysql.session", "localhost"),
+        ("mysql.sys", "localhost"),
+        ("root", "localhost"),
+    ],
+    "QUERY_12_LIST_USERS_AND_ITS_PERMISSIONS": [
+        ("root",),
+        ("mysql.infoschema",),
+        ("mysql.session",),
+        ("mysql.sys",),
+        ("root",),
+    ],
+}
 
 
 def join_tuple_elements(t):
     return ".".join(t)
 
 
-def show_tuples_diff(source_list:list = [], destiny_list:list = [], source_name:str = "blue", destiny_name:str = "green"):
+def compare_lists(
+    source_list: list = [],
+    destiny_list: list = [],
+    source_name: str = "blue",
+    destiny_name: str = "green",
+):
     set_l1 = set(source_list)
     set_l2 = set(destiny_list)
 
@@ -81,13 +120,17 @@ def show_tuples_diff(source_list:list = [], destiny_list:list = [], source_name:
 
     # Print the output
     output_content = f"{source_name},{destiny_name},result"
-    for pair in output:
-        output_content = output_content + "\n" + pair[0] + "," +  pair[1] + "," + pair[2]
+    for item in output:
+        output_content = output_content + "\n" + item[0] + "," + item[1] + "," + item[2]
     return output_content
+
 
 """ Few are just list others are key value """
 
-def compare_dicts(dict_a: dict, dict_b: dict, source_name:str = "blue", destiny_name:str = "green") -> str:
+
+def compare_dicts(
+    dict_a: dict, dict_b: dict, source_name: str = "blue", destiny_name: str = "green"
+) -> str:
     all_keys = set(dict_a.keys()).union(set(dict_b.keys()))
     result = f"key,{source_name},{destiny_name},result"
     for key in all_keys:
@@ -99,15 +142,17 @@ def compare_dicts(dict_a: dict, dict_b: dict, source_name:str = "blue", destiny_
             status = "equal"
         else:
             status = "different"
-        result = result +  "\n"
+        result = result + "\n"
         result = result + f'"{key}","{value_a}","{value_b}",{status}'
     return result
+
 
 def extract_dict_from_list(l: set):
     output = {}
     for i in l:
         output[i[0]] = i[1]
     return output
+
 
 # key_value diff
 def kv_diff(source: list, destiny: list) -> str:
@@ -116,26 +161,31 @@ def kv_diff(source: list, destiny: list) -> str:
     return compare_dicts(source_dict, destiny_dict)
 
 
+def compare_output_general_phase(output_src: dict, output_dest: dict, show_diff: bool):
+    all_keys = set(output_src.keys()).union(set(output_dest.keys()))
+    for key in sorted(all_keys):
+        if tuples_list_are_equal(output_src[key], output_dest[key]):
+            print(f"{key} are igual")
+        else:
+            print(f"{key} are different")
+
+        if show_diff:
+            # If are key value
+            if key in ["QUERY_02_VARIABLES", "QUERY_11_LIST_USERS_AND_HOSTS"]:
+                source_dict = extract_dict_from_list(set(output_src[key]))
+                destiny_dict = extract_dict_from_list(set(output_dest[key]))
+                print_with_indent(compare_dicts(source_dict, destiny_dict), 1)
+            # If elements are just list
+            else:
+                print_with_indent(
+                    compare_lists(
+                        source_list=output_src[key], destiny_list=output_dest[key]
+                    ),
+                    1,
+                )
 
 
-all_keys = set(output_source.keys()).union(set(output_destiny.keys()))
-for key in sorted(all_keys):
-    if tuples_list_are_equal(output_source[key], output_destiny[key]):
-        print(f"{key} are igual")
-    else:
-        print(f"{key} are different")
-        # If are key value
-    if key in ["QUERY_02_VARIABLES", "QUERY_11_LIST_USERS_AND_HOSTS"]:
-        if SHOW_DIFF:
-            source_dict = extract_dict_from_list(set(output_source[key]))
-            destiny_dict = extract_dict_from_list(set(output_destiny[key]))
-            print_with_indent(compare_dicts(source_dict, destiny_dict), 1)
-    # If elements are just list
-    else:
-        if SHOW_DIFF:
-            print_with_indent(show_tuples_diff(source_list=output_source[key], destiny_list=output_destiny[key]), 1)
-            # show_diff(
-            #     output_source[key],
-            #     output_destiny[key],
-            #     fetch_list_from_set
-            # )
+if __name__ == "__main__":
+    compare_output_general_phase(
+        output_src=output_source, output_dest=output_destiny, show_diff=False
+    )
