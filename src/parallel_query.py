37f81eaafe002539e4ae3c60ba927979b0d1eaf1
Tiago <tiagotele@gmail.com>
2024-10-24 15:18:53 -0300

    Formatting

--- src/parallel_query.py
+++ src/parallel_query.py
@@ -5,7 +5,7 @@ MAX_WORKERS = 32
 
 
 def run_completed(futures):
-    result=[]
+    result = []
     for future in as_completed(futures):
         table = futures[future]
         try:
@@ -25,10 +25,12 @@ def execute_queries_in_parallel(
     print("Starting queries")
     with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
         future_to_table_src = {
-            executor.submit(run_query, config_src, name, query): (name, query) for name, query in queries_src.items()
+            executor.submit(run_query, config_src, name, query): (name, query)
+            for name, query in queries_src.items()
         }
         future_to_table_dest = {
-            executor.submit(run_query, config_dest, name, query): (name, query) for name, query in queries_dest.items()
+            executor.submit(run_query, config_dest, name, query): (name, query)
+            for name, query in queries_dest.items()
         }
 
         output_src = run_completed(future_to_table_src)
